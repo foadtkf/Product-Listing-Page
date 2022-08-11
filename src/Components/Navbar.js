cde97944df067ae237c8b1370b4ba5907b96d44f
foadtkf <fakhrulif@gmail.com>
2022-08-11 12:41:57 +0600

    navbar updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,31 +4,13 @@ const Navbar = () => {
     return (
         <div class="navbar bg-base-100">
   <div class="navbar-start">
-    <div class="dropdown">
-      <label tabindex="0" class="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li tabindex="0">
-          <a class="justify-between">
-            Parent
-            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-          </a>
-          <ul class="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
+    
     <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
   </div>
   <div class="navbar-center hidden lg:flex">
     <ul class="menu menu-horizontal p-0">
     <li>
-      <input type="text" placeholder="Search" class="input input-bordered" />
+      <input type="text" placeholder="Search" class="input input-bordered my-auto" />
     </li>
       <li><div class="dropdown">
       <label tabindex="0" class="btn btn-ghost btn-circle">
@@ -40,17 +22,17 @@ const Navbar = () => {
         <li><a>About</a></li>
       </ul>
     </div></li>
-      <li tabindex="0">
-        <a>
-          Parent
-          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul class="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
-      </li>
-      <li><a>Item 3</a></li>
+    <button class="btn btn-ghost btn-circle my-auto">
+      <div class="indicator">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+        <span class="badge badge-xs badge-primary indicator-item"></span>
+      </div>
+    </button>
+    <label tabindex="0" class="btn btn-ghost btn-circle avatar my-auto">
+        <div class="w-10 rounded-full">
+          <img src="https://placeimg.com/80/80/people" alt=''/>
+        </div>
+      </label>
     </ul>
   </div>
   <div class="dropdown navbar-end block lg:hidden">
@@ -62,6 +44,17 @@ const Navbar = () => {
         <li><a>Portfolio</a></li>
         <li><a>About</a></li>
       </ul>
+      <button class="btn btn-ghost btn-circle">
+      <div class="indicator">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+        <span class="badge badge-xs badge-primary indicator-item"></span>
+      </div>
+    </button>
+    <label tabindex="0" class="btn btn-ghost btn-circle avatar my-auto">
+        <div class="w-10 rounded-full">
+          <img src="https://placeimg.com/80/80/people" alt=''/>
+        </div>
+      </label>
     </div>
 </div>
     );
